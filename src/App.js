115daf4689dc905a2db0867beacdbcc4f0abe05e
Andi Javier Lafiedrido <andijavier442@gmail.com>
2022-01-18 16:22:13 +0700

    stepAway v3.0.0

--- src/App.js
+++ src/App.js
@@ -1,17 +1,61 @@
-import logo from './ciggarete.gif';
+import ciggarete from './ciggarete.gif';
+import coffee from './coffee.gif'
+import React, { useState } from 'react'
 import './App.css';
 
 function App() {
+  const [sentenceOne, setSentenceOne] = useState(localStorage.getItem('sentenceOne')? localStorage.getItem('sentenceOne') : 'Step Away For A Smoke')
+  const [sentenceTwo, setSentenceTwo] = useState(localStorage.getItem('sentenceTwo')? localStorage.getItem('sentenceTwo') : 'See you on bottom')
+  const [editOne, setEditOne] = useState(false)
+  const [editTwo, setEditTwo] = useState(false)
+  const [listLogo] = useState([{ image: ciggarete, index: 0 }, { image: coffee, index: 1 }])
+  const [logo, setLogo] = useState(localStorage.getItem('logo')? listLogo[localStorage.getItem('logo')] : { image: ciggarete, index: 0 })
+
+  function editSentenceOne () {
+    setEditOne(true)
+  }
+
+  function editSentenceTwo () {
+    setEditTwo(true)
+  }
+
+  function changeLogo () {
+    if (logo.index < listLogo.length - 1) {
+      setLogo(listLogo[logo.index + 1])
+      localStorage.setItem('logo', logo.index + 1)
+    } else {
+      setLogo(listLogo[0])
+      localStorage.setItem('logo', 0)
+    }
+  }
+
+  function save () {
+    localStorage.setItem('sentenceOne', sentenceOne)
+    localStorage.setItem('sentenceTwo', sentenceTwo)
+    setEditOne(false)
+    setEditTwo(false)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="stepAway">
-          Step Away For A Smoke
-        </p>
-        <p className="seeYou">
-          See you on bottom
-        </p>
+        {
+          (editOne || editTwo) ? <button className="save btn" onClick={save}>SAVE</button> :
+          ''
+        }
+        <img src={logo.image} onClick={changeLogo} className="App-logo" alt="logo" />
+        { 
+          editOne ? <input className="stepAway input" type="text" value={sentenceOne} onChange={ (sentence) => setSentenceOne(sentence.target.value) } /> : 
+          <p className="stepAway" onClick={editSentenceOne}>
+            { sentenceOne }
+          </p>  
+        }
+        { 
+          editTwo ? <input className="seeYou input" type="text" value={sentenceTwo} onChange={ (sentence) => setSentenceTwo(sentence.target.value) } /> : 
+          <p className="seeYou" onClick={editSentenceTwo}>
+            { sentenceTwo }
+          </p>
+        }
       </header>
     </div>
   );
