cdd90b63d9d2473b286c199ad06ec1fca9c82117
Andi Javier Lafiedrido <andijavier442@gmail.com>
2022-01-18 11:31:00 +0700

    add README.md

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './ciggarete.gif';
 import './App.css';
 
 function App() {
@@ -6,17 +6,12 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p className="stepAway">
+          Step Away For A Smoke
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p className="seeYou">
+          See you on bottom
+        </p>
       </header>
     </div>
   );
